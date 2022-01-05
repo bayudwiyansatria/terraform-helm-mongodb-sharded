40f6a592526d80332153b5a2e84a3839a2d4cba0
Bayu Dwiyan Satria <bayudwiyansatria@gmail.com>
2022-01-06 01:44:48 +0800

    Add outputs description

--- outputs.tf
+++ outputs.tf
@@ -1,23 +1,26 @@
-output "postgresql_version" {
-  sensitive  = false
-  value      = var.mongodb_version
-  depends_on = [
+output "mongodb_version" {
+  sensitive   = false
+  value       = var.mongodb_version
+  description = "MongoDB Version"
+  depends_on  = [
     helm_release.mongodb-sharded
   ]
 }
 
 output "username" {
-  sensitive  = false
-  value      = "root"
-  depends_on = [
+  sensitive   = false
+  value       = "root"
+  description = "MongoDB username for login"
+  depends_on  = [
     helm_release.mongodb-sharded
   ]
 }
 
 output "password" {
-  sensitive  = true
-  value      = var.mongodb_password
-  depends_on = [
+  sensitive   = true
+  description = "MongoDB password for login"
+  value       = var.mongodb_password
+  depends_on  = [
     helm_release.mongodb-sharded
   ]
 }
